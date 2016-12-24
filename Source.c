2d4124be112c353e13692e1005a45dfda492fdaa
akkineniramesh <akkineniramesh@gmail.com>
2016-12-24 13:06:48 +0530

    c SnakenLadders code started

--- Source.c
+++ Source.c
@@ -2,7 +2,38 @@
 #include <stdlib.h>
 int multiply(int m1, int m2);
 int Dice(int pos);
-int pos = 1;
+int pos = 1, turnsofdice = 0, turnsofgame = 0;
+int a[101];
+long numberofdiceturns = 10000;
+void runGame(), TurnsofDice(), fillLadders(), fillSnakes(), numberofGames(), printGameStats();
+int SnakesLadders();
+int ladderStart[] = { 8,19,21,28,36,43,50,54,61,62,66 };
+int ladderEnd[] = { 26,38,82,53,57,77,91,88,99,96,87 };
+int snakeStart[] = { 98,93,83,69,68,64,59,52,48,46 };
+int snakeEnd[] = { 13,37,22,33,2,24,18,11,9,15 };
+int ladderHit[12];
+int snakeHit[11];
+int lIndex[101];
+int sIndex[101];
+int turnsofdiceforagame = 0;
+long turnsasmoney = 10000;
+float basispoint = 100;
+/*	int pos=1,turnsofdice=0,turnsofgame=0;
+	int a[]=new int[101];
+	int ladderStart[]={8,19,21,28,36,43,50,54,61,62,66};
+	int ladderEnd[] = {26,38,82,53,57,77,91,88,99,96,87};
+	int snakeStart[]= {98,93,83,69,68,64,59,52,48,46};
+	int snakeEnd[] =  {13,37,22,33,2,24,18,11,9,15};
+	int ladderHit[]=new int[11];
+	int snakeHit[]= new int[10];
+	int lIndex[]= new int[101];
+	int sIndex[]= new int[101];
+	int numberofgames=0;
+	long numberofdiceturns;
+	int turnsofdiceforagame=0;
+	double turnsasmoney=0;
+	float basispoint=100;
+	Random r= new Random();*/
 main()
 {
 	int dice;
@@ -21,10 +52,10 @@ main()
 	int ndice[10];
 	for(dice1=0;dice1<10;++dice1)
 	{
-		ndice[dice1] = 5;
+		ndice[dice1] = dice1*2;
 		printf("%d", ndice[dice1]);
 	}
-	if (dice1 > dice)printf("%d %d", dice1, dice);
+	if (dice1 > dice)printf(" %d %d", dice1, dice);
 	printf(" %d", multiply(5, 3));
 
 	srand(time(NULL));
@@ -36,6 +67,8 @@ main()
 		//pos=Dice(pos);
 		Dice(pos);
 	}
+	runGame();
+	printGameStats();
 	printf("hello ramesh\n");
 	getchar();
 }
@@ -48,6 +81,91 @@ int multiply(int m1,int m2)
 int Dice(int p)
 {
 	pos = p + rand() % 6 +1;
-	printf(" %d", pos);
+	//printf(" %d", pos);
 	return pos;
+}
+void runGame()
+{
+	for (int i = 1; i<101; i++)
+	{
+		a[i] = 0;
+		lIndex[i] = 0;
+		sIndex[i] = 0;
+	}
+	for (int i = 1; i < 12; i++)ladderHit[i] = 0;
+	for (int i = 1; i < 11; i++)snakeHit[i] = 0;
+	//getSnakesLadders(args);
+	//System.out.println("turnsasmoneyatstart=" + turnsasmoney);
+	fillLadders();
+	fillSnakes();
+	while (turnsofdice<numberofdiceturns)
+	{
+		pos = Dice(pos);
+		TurnsofDice();
+		if (pos>100)numberofGames();
+		pos = SnakesLadders();
+	}
+}
+void TurnsofDice()
+{
+	++turnsofdice;
+	++turnsofdiceforagame;
+}
+int SnakesLadders()
+{
+	if (a[pos] != 0)
+	{
+		if (lIndex[pos] != 0)ladderHit[lIndex[pos]]++;
+		if (sIndex[pos] != 0)snakeHit[sIndex[pos]]++;
+		//if(pos>a[pos])printf("snakehit %d",pos);
+		//if(pos<a[pos])printf("ladderhit %d",pos);
+		pos = a[pos];
+	}
+	return pos;
+}
+void fillLadders()
+{
+	for (int i = 0; i<11; i++)
+	{
+		//if (lIndex[ladderStart[i]] != 0)
+		//{
+			a[ladderStart[i]] = ladderEnd[i];
+			lIndex[ladderStart[i]] = i + 1;
+		//}
+	}
+}
+void fillSnakes()
+{
+	for (int i = 0; i<10; i++)
+	{
+		//if (sIndex[snakeStart[i]] != 0)
+		//{
+			a[snakeStart[i]] = snakeEnd[i];
+			sIndex[snakeStart[i]] = i + 1;
+		//}
+	}
+}
+void numberofGames()
+{
+	pos = pos - 100;
+	//System.out.println("turnsofgame="+turnsofgame);
+	//System.out.println("position="+pos);
+	turnsofgame++;
+	turnsasmoney = turnsasmoney*(((10000 - basispoint) / 10000) + (basispoint / (turnsofdiceforagame * 350)));
+	turnsofdiceforagame = 0;
+	//if(turnsofgame%35==0)numberofdiceturns=numberofdiceturns+100;
+}
+void printGameStats()
+{
+	printf("turnsofdice= %d \n" , turnsofdice);
+	printf("turnsofgame= %d \n" , turnsofgame);
+	printf("numberofdiceturns= %d \n" , numberofdiceturns);
+	printf("turnsofdiceindistance= %f \n" , turnsofdice*3.5);
+	printf("turnsasmoney= %u \n" , turnsasmoney);
+	//System.out.println("numberofdiceturnsindistance="+(long)(numberofdiceturns*3.5));
+	printf("pos=" + pos);
+	for (int i = 1; i <= 11; i++)
+		printf("LadderHit%d = %d" , i , ladderHit[i]);
+	for (int i = 1; i <= 10; i++)
+		printf("SnakeHit%d = %d" , i , snakeHit[i]);
 }
